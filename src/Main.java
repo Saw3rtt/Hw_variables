943c37fa65e85e731c87b93865a116a80829e1e3
Saw3rtt <sawertpra@gmail.com>
2023-05-25 10:27:56 +0300

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,13 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println();
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
